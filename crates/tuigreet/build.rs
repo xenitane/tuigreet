47a74aaee42f53fbe1302a9592aec0d71571d347
NotAShelf <raf@notashelf.dev>
2026-06-13 13:55:27 +0300

    various: move locales into `tuigreet-locales` crate
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I0c77544a14116e8dac514ce6a3a669bd6a6a6964

--- crates/tuigreet/build.rs
+++ crates/tuigreet/build.rs
@@ -1,13 +1,17 @@
 use std::{env, process::Command};
 
 fn main() {
-  let version = git_version().unwrap_or_else(|| env!("CARGO_PKG_VERSION").to_string());
+  let version =
+    git_version().unwrap_or_else(|| env!("CARGO_PKG_VERSION").to_string());
   println!("cargo:rustc-env=VERSION={version}");
   println!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
 }
 
 fn git_version() -> Option<String> {
-  let out = Command::new("git").args(["describe", "--long"]).output().ok()?;
+  let out = Command::new("git")
+    .args(["describe", "--long"])
+    .output()
+    .ok()?;
   if !out.status.success() {
     return None;
   }
